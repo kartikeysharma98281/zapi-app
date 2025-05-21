c9471e723aae2d865bf67a8078da4eb39fd27a9f
kartikeysharma98281 <kartikeysharma.sharma@gmail.com>
2025-05-21 15:19:50 +0530

    change the title

--- frontend/components/AppBar.tsx
+++ frontend/components/AppBar.tsx
@@ -7,7 +7,7 @@ export const AppBar = () => {
     const router = useRouter();
     return <div className="flex border-b justify-between p-4">
         <div className="flex flex-col justify-center text-2xl font-extrabold">
-            Zapier
+            Zapi
         </div>
         <div className="flex">
             <div className="pr-4">
